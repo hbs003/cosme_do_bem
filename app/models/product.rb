0e3e949fe1505fc6268c79a331c1ffe4b51eaddd
Henrique Silva <hbs003@hotmail.com>
2018-06-20 02:12:37 +0200

    mamam

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,14 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :orders, through: :order_products
   monetize :price_cents
+
+  include PgSearch
+  pg_search_scope :global_search,
+    against: [ :name, :sku ],
+    associated_against: {
+      category: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
